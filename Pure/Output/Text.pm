7761991d34c125c28ee79449fce1eefea664943d
Michal Špaček <skim@cpan.org>
2012-04-27 10:17:49 +0000

    Import of SKIM/Error-Pure-0.06 from CPAN.
    gitpan-cpan-distribution: Error-Pure
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SKIM/Error-Pure-0.06.tar.gz
    gitpan-cpan-author:       SKIM
    gitpan-cpan-maturity:     released

--- Pure/Output/Text.pm
+++ Pure/Output/Text.pm
@@ -13,7 +13,7 @@ Readonly::Array our @EXPORT_OK => qw(err_bt_pretty err_line err_line_all);
 Readonly::Scalar my $SPACE => q{ };
 
 # Version.
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Pretty print of backtrace.
 sub err_bt_pretty {
@@ -33,7 +33,7 @@ sub err_bt_pretty {
 				last;
 			}
 			$ret .= $f;
-			if ($t) {
+			if (defined $t) {
 				$ret .= ': '.$t;
 			}
 			$ret .= "\n";
@@ -324,6 +324,6 @@ BSD license.
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
